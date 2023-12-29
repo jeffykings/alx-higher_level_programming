8e9b7b31894e256a16adc22ffd79e6165f839ec1
JK7 <jeffykings@gmail.com>
2023-12-29 15:23:07 +0000

    task10 update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,10 +9,13 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
+	if (list == NULL)
+		return (0);
+
 	slow = list;
 	fast = list;
 
-	while (slow != NULL && fast != NULL)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
