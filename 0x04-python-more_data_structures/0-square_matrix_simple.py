04d7695b4d12c4cdebd8504bbd86721018c53774
JK7 <jeffykings@gmail.com>
2024-01-06 08:39:35 +0100

    pycodestyle fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@
 def square_matrix_simple(matrix=[]):
     newmatrix = []
     for i in matrix:
-        newlist = list(map(lambda x : x**2, i))
+        newlist = list(map(lambda x: x**2, i))
         newmatrix.append(newlist)
     return (newmatrix)
