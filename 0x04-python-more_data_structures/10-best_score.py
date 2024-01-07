5ae796fcb41aa18b5f6081fe0e31c31aec2e6ca5
JK7 <jeffykings@gmail.com>
2024-01-07 01:13:00 +0100

    updated 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary == {} or a_dictionary is None:
         return (None)
     dict_values = list(a_dictionary.values())
     tempvalue = dict_values[0]
