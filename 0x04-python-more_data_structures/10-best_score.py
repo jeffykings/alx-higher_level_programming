8421d8278b4bae258b33ad365af5fb6cc64facff
JK7 <jeffykings@gmail.com>
2024-01-07 01:10:34 +0100

    task10 update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,7 +8,7 @@ def best_score(a_dictionary):
 
     for key in a_dictionary.keys():
 
-        if a_dictionary[key] > tempvalue:
+        if a_dictionary[key] >= tempvalue:
             tenpvalue = a_dictionary[key]
             highest_key = key
 
