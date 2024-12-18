358665ce5bd3fa7b396cfa49f810c240c0693d43
jeffykings <jeffykings@gmail.com>
2024-12-18 11:31:38 +0100

    task12 pycode_update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,17 @@
 
 def roman_to_int(roman_string):
 
-    if roman_string == "" or roman_string == None:
+    if roman_string == "" or roman_string is None:
         return 0
 
-    roman_num =  {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_num = {
+            "I": 1, "V": 5, "X": 10, "L": 50,
+            "C": 100, "D": 500, "M": 1000
+            }
+
     temp = roman_num[roman_string[0]]
     sum_roman = 0
-    
+
     if len(roman_string) == 1:
         return temp
 
@@ -24,7 +28,6 @@ def roman_to_int(roman_string):
 
                 temp = roman_num[roman_string[value]]
             else:
-                print("wrong input")
                 return (0)
 
         sum_roman += temp
