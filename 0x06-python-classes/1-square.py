d702b61206c9df5f657d4858072966a1ceccfa71
jeffykings <jeffykings@gmail.com>
2025-01-30 15:03:01 +0100

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" This contains a class Square"""
+
+
+class Square:
+    """Initiates the size of a class"""
+
+    def __init__(self, size):
+        """Runs a class is called to initiate a class"""
+        self.__size = size
