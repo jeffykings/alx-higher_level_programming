f5d49d093518a00902ce6a88a02a4fa6aec4a585
jeffykings <jeffykings@gmail.com>
2025-01-30 15:34:30 +0100

    task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" This contains a class Square"""
+
+
+class Square:
+    """Initiates the size of a class"""
+
+    def __init__(self, size=0):
+        """Runs a class is called to initiate a class"""
+
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
