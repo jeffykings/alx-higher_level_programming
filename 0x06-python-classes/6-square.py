ac2b1e421d5f4a99a7e054cf5d684994ca04da17
jeffykings <jeffykings@gmail.com>
2025-01-30 16:35:13 +0100

    task6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+""" This contains a class Square"""
+
+
+class Square:
+    """Initiates the size of a class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Runs a class is called to initiate a class"""
+
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """getter for size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+
+        if not (isinstance(value, int)):
+            raise TypeError("size must be an integer")
+
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        """ getter for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ setter for position"""
+
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """ Returns the area of the square"""
+
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+
+                counter = self.__position[0]
+                while (counter):
+                    print(" ", end="")
+                    counter -= 1
+
+                counter = self.__size
+                while (counter):
+                    print("#", end="")
+                    counter -= 1
+                print()
