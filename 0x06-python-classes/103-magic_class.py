b91e9d9e35e5d318132ed4947b1055d19a3e6e2b
jeffykings <jeffykings@gmail.com>
2025-01-31 01:09:01 +0100

    task103

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/pythton3
+
+"""class Magic"""
+
+
+class MagicClass:
+    """ contains method init, area and circumference"""
+
+    def __init__(self, radius):
+        if not isinstance(radius, int) or not isinstance(radius, float):
+            raise TypeError("radius must be a number")
+
+        self._MagicClass__radius = radius
+
+    def area(self):
+        """area of a circle"""
+
+        return (math.pi * (self._MagicClass__radius ** 2))
+
+    def circumference(self):
+        """calculate circumference of a circle"""
+
+        return (2 * math.pi * self._MagicClass__radius)
