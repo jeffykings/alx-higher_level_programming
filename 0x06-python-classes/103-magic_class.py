ecfe5505bbbddf871ae5e5ce25a5ff868cb930fb
jeffykings <jeffykings@gmail.com>
2025-01-31 01:29:10 +0100

    task103_fix3

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,18 +7,19 @@ import math
 class MagicClass:
     """ contains method init, area and circumference"""
 
-    def __init__(self, radius):
-        if not isinstance(radius, int) or not isinstance(radius, float):
+    def __init__(self, radius=0):
+
+        if not isinstance(radius, int) or (not isinstance(radius, float)):
             raise TypeError("radius must be a number")
 
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
         """area of a circle"""
 
-        return (math.pi * (self._MagicClass__radius ** 2))
+        return (math.pi * (self.__radius ** 2))
 
     def circumference(self):
         """calculate circumference of a circle"""
 
-        return (2 * math.pi * self._MagicClass__radius)
+        return (2 * math.pi * self.__radius)
---
+++ 0x06-python-classes/103-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+MagicClass = __import__('103-magic_class').MagicClass
+
+my_square = MagicClass(5)
+print(my_square)
+
+print("--")
+
+my_square2 = MagicClass(10)
+print(my_square2)
