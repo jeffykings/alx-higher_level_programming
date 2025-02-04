dfd7c2f2ce874677103d361ca4e111bd3eb867ce
jeffykings <jeffykings@gmail.com>
2025-02-04 21:55:48 +0100

    task3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""function that prints My name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints a name
+    Args:
+    first_name: must be a string
+    last_name: must be a string
+
+    Returns:
+         My name is <first name> <last name>
+    Raises:
+        TypeError: first_name must be a string or
+        last_name must be a string if any is not string
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
