f2378b7b1e43dede0aa6dd2572c089fcc34096a2
jeffykings <jeffykings@gmail.com>
2025-02-04 22:57:28 +0100

    fix3 task4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -20,9 +20,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size == 0:
-        print()
-
     else:
         for i in range(size):
             print("#" * size)
