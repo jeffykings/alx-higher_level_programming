46174987f723092e5e3fe57ec6014fe6b65a080e
jeffykings <jeffykings@gmail.com>
2025-03-02 21:43:59 +0100

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" a class that contains a function area """
+
+
+class BaseGeometry:
+    """contains area"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
