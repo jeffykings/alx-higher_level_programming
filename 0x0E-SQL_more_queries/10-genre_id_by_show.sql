00f33d1f63d004425675e5ddd49c27ff3c31174b
jeffykings <jeffykings@gmail.com>
2026-01-16 20:53:57 +0100

    task11

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,14 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download
+
+-- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT s.title, sg.genre_id
+FROM tv_show_genres AS sg
+JOIN tv_shows AS s
+ON sg.show_id = s.id
+ORDER BY s.title ASC, sg.genre_id ASC;
