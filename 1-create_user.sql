edc99ef4a28abdc0229a6b466f4376bb8adcbedd
jeffykings <jeffykings@gmail.com>
2026-01-16 13:38:12 +0100

    task1

---
+++ 1-create_user.sql
@@ -0,0 +1,15 @@
+-- a script that creates the MySQL server user user_0d_1.
+-- user_0d_1 should have all privileges on your MySQL server
+-- The user_0d_1 password should be set to user_0d_1_pwd
+-- If the user user_0d_1 already exists, your script should not fail
+
+
+-- Create user if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on the server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes
+FLUSH PRIVILEGES;
